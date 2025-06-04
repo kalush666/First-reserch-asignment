e3e8babb70a63400364edc8c6e9a40a5d593c237
kalush666 <jonathan.kalush@gmail.com>
2025-06-04 19:34:45 +0300

    patient class

---
+++ backend-csharp/backend-csharp/Models/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace backend_csharp.Models
+{
+    public class Patient : User
+    {
+        private List<string> Allergies { get; set; } = new();
+        private List<string> Medications { get; set; } = new();
+
+        public Patient(int id, string name, decimal age, List<string> allergies,List<string> medications) 
+            : base(id, name, age) {
+            this.Allergies = allergies;
+            this.Medications = medications;
+        }
+
+        public Patient(int id, string name, decimal age)
+            : base(id, name, age) { }
+
+        public override string ToString()
+        {
+            string allergies = string.Join(", ", Allergies);
+            string medications = string.Join(", ", Medications);
+
+            return base.ToString() + $" | Allergies: [{allergies}] | Medications: [{medications}]";
+        }
+    }
+}
---
+++ backend-csharp/backend-csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace backend_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            var query = from n in numbers
+                        where n % 2 == 0
+                        select n;
+
+            foreach (var val in query) {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
