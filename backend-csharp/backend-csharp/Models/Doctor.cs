a77237cee7e68a4747ab0662611d4677461c50f9
kalush666 <jonathan.kalush@gmail.com>
2025-06-04 19:24:41 +0300

    Doctor class that wil inherit from User

---
+++ backend-csharp/backend-csharp/Models/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using backend_csharp.Models.Enums;
+
+namespace backend_csharp.Models
+{
+    public class Doctor : User
+    {
+        private DoctorSpeciality Speciality { get; set; }
+
+        public Doctor(int id, string name, decimal age, DoctorSpeciality speciality)
+            : base(id, name, age) {
+            this.Speciality = speciality;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Speciality {Speciality}";
+        }
+    }
+}
