5ea8d8728d3402e14d427f66c228c8d0730ba54c
kalush666 <jonathan.kalush@gmail.com>
2025-06-04 21:42:53 +0300

    Appointment status enum

---
+++ backend-csharp/backend-csharp/Models/Enums/AppointmentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_csharp.Models.Enums
+{
+    public  enum ApointmentStatus{ 
+        Pending,
+        Confirmed,
+        Completed,
+        Cancelled,
+        NoShow
+    }
+}
