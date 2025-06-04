9ab1bfc83c8ffd0132d31de66c9993dcda1510c2
kalush666 <jonathan.kalush@gmail.com>
2025-06-04 19:21:53 +0300

    improvments for user class

---
+++ backend-csharp/backend-csharp/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace backend_csharp.Models
+{
+    public abstract class User
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private decimal Age { get; set; }
+
+        public User(int id,string name,decimal age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public virtual string ToString()
+        {
+            return $"Id:{Id} Name:{Name} Age:{Age}";
+        }
+    }
+}
