f40faf54b896e8ed8a743e587b02036a7fed4b4e
kalush666 <jonathan.kalush@gmail.com>
2025-06-04 19:43:25 +0300

    appointment reasons

---
+++ backend-csharp/backend-csharp/Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend_csharp.Models
+{
+    public class Appointment
+    {
+        private Doctor Doctor { get; set; }
+        private Patient Patient { get; set; }
+        private DateTime AppointmentDateTime { get; set; }
+
+        private List<string> AppointmentReasons { get; set; }
+        public Appointment(Doctor doctor, Patient patient,DateTime appointmentDateTime,List<string> appointmentReasons) {
+            this.Doctor = doctor;
+            this.Patient = patient;
+            this.AppointmentDateTime = appointmentDateTime;
+            this.AppointmentReasons = appointmentReasons;
+        }
+    }
+}
