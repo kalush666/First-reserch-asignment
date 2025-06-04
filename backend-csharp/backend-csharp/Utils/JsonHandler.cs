c4537cae248c56ebef8d5400ecc30002d3fd1302
kalush666 <jonathan.kalush@gmail.com>
2025-06-04 21:31:15 +0300

    jsonHandler from sending and getting json with the udp and tcp protocols

---
+++ backend-csharp/backend-csharp/Utils/JsonHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace backend_csharp.Utils
+{
+    class JsonHandler
+    {
+        public static string ToJson<T>(T obj)
+        {
+            try { 
+                return JsonConvert
+            }
+        }
+    }
+}
