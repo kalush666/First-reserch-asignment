7a583143ae32380b8038b65a1a31345ba80f65cb
kalush666 <jonathan.kalush@gmail.com>
2025-06-06 19:01:22 +0300

    Config module

--- backend-nest-js/src/prisma/prisma.service.ts
+++ backend-nest-js/src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:jonathan06@localhost:5432/appointment_db?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
